1024132a9492f4913785fc7495a43ba80a75d6ae
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2011-08-22 17:59:56 -0430

    Mejorando el script de conversión

--- convertir-a-cunaguaro.sh
+++ convertir-a-cunaguaro.sh
@@ -11,14 +11,18 @@ for REEMPLAZAR in ${OPCIONES}; do
 	ICEWEASEL=${REEMPLAZAR%_____________________*}
 	CUNAGUARO=${REEMPLAZAR#${ICEWEASEL}_____________________}
 
-	for ARCHIVO in $( find ${CURRENT} -type f ); do
-
-		mv ${ARCHIVO} $( echo ${ARCHIVO} | sed "s/${ICEWEASEL}/${CUNAGUARO}/g" )
-
-		sed -i "s/${ICEWEASEL}/${CUNAGUARO}/g" ${ARCHIVO}
-
+	for ARCHIVO in $( find . -type f | grep -v ".git/" ); do
+		NUEVO=$( echo ${ARCHIVO} | sed "s/${ICEWEASEL}/${CUNAGUARO}/g" )
+		if [ "${ARCHIVO}" != "${NUEVO}" ]; then
+			echo "Renombrando ${ARCHIVO} a ${NUEVO}"
+			mv ${ARCHIVO} ${NUEVO}
+		fi
 	done
 
+	for ARCHIVO in $( grep -IHR "${ICEWEASEL}" . | cut -d: -f1 | grep -v ".git/" ); do
+		echo "Sustituyendo ${ICEWEASEL} por ${CUNAGUARO} en ${ARCHIVO}"
+		sed -i "s/${ICEWEASEL}/${CUNAGUARO}/g" ${ARCHIVO}
+	done
 done
 
 
