75a0d2be9a1d733f50af52059bc51368cc7105dc
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2013-08-28 14:21:50 -0430

    Removiendo fuente para Cunaguaro 22.

--- debian/branding/configure.sh
+++
@@ -1,3 +0,0 @@
-MOZ_APP_BASENAME="Cunaguaro"
-MOZ_APP_DISPLAYNAME="Cunaguaro"
-MOZ_APP_PROFILE=mozilla/firefox
--- debian/branding/firefox-branding.js
+++
@@ -1,3 +0,0 @@
-pref("startup.homepage_override_url", "http://canaima.softwarelibre.gob.ve/");
-pref("startup.homepage_welcome_url", "http://canaima.softwarelibre.gob.ve/");
-pref("app.releaseNotesURL", "http://canaima.softwarelibre.gob.ve/");
